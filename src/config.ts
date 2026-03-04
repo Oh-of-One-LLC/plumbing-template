6ad8f66d5cc08a9063f11f84542319a7deca6cc8
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2026-03-04 14:42:16 -0700

    add footer with contact info

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
-export const COMPANY_NAME = "Smith & Sons Plumbing";
-export const PHONE = "(555) 123-4567";
+const COMPANY_NAME = "Smith & Sons Plumbing";
+const PHONE = "(555) 123-4567";
+const ADDRESS = "1801 Wynkoop Street, Denver, CO 80202";
+
+export { COMPANY_NAME, PHONE, ADDRESS };
